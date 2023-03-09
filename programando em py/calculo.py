4fc502e904b52753bc0c2dd08e644b3ef627bd61
jalviminf <jalvim.inf@gmail.com>
2023-03-08 21:24:38 -0300

    first commit

---
+++ programando em py/calculo.py
@@ -0,0 +1,8 @@
+'''
+crie um programa que receba dois numeros inteiros e depois mostre o resultado da soma e da multriplocaçao de um numero pelo putro
+'''
+numero1 = int (input("informe um número"))
+numero2 = int (input("informe outro número"))
+resultado_soma = numero1 + numero2
+resultado_multiplicao = numero1 * numero2
+print("A soma foi ", resultado_soma, "e a multiplicaçao foi" , resultado_multiplicao)
