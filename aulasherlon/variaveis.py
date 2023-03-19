b95af9c761e02e381b3433c8862d2ae210e0b47e
jalviminf <jalvim.inf@gmail.com>
2023-03-19 17:09:41 -0300

    Main

---
+++ aulasherlon/variaveis.py
@@ -0,0 +1,45 @@
+#variaveis
+#nome = "jorge" #tipo string(str)
+#nomenumero = 42      #tipo iteiro(int)
+#numero_com_virgula = 119.99 #tipo float numero com virgula (usar . ponto ) (float)
+
+#print ("o seu nome é :", nome) #permitir a visualizacao das informaçoes  
+
+#a = 9
+#b = 10
+#x = (a + b)
+#print(x)
+
+
+#input
+#interaçao com usuario
+
+#nome = input("vc poderia digitar seu nome:")
+#print("que nome feio esse :" , nome )
+
+#a = int(input("digite o valor de a:"))
+#b = int(input("digite o valor de b:"))
+#resposta = (a + b)
+#print(resposta)
+
+#resposta abaixo bee
+
+'''A = int(input())
+B = int(input())
+X = (A + B)
+print('X =', A + B)'''
+
+'''raio = float(input())
+n = 3.14159
+area = n*raio*raio
+print ('A=%.4f' % area)''' 
+
+n = int(input('Digite um número qualquer: '))
+
+if n % 2 == 0:
+
+    print('O número é PAR!')
+
+else:
+
+    print('O número é ÍMPAR!')
