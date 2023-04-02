1e0eaab4c921b349a17b7164eba81ce99666832c
jalviminf <jalvim.inf@gmail.com>
2023-04-02 14:25:41 -0300

    Main

---
+++ trabalhando 3/tes.py
@@ -0,0 +1,13 @@
+'''print("Hello World!")'''
+
+'''A = int(input())
+B = int(input())
+
+print ('X = ', A + B)'''
+
+
+'''raio = float(input())
+n = 3.14159
+area = n*raio*raio
+print(f"A=",area)'''
+
